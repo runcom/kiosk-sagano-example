c8cb3e3db44d7f4ae3676804225edcf56f4610b1
Antonio Murdaca <antoniomurdaca@gmail.com>
2023-11-27 08:38:31 +0100

    conditionally download boot.iso
    Signed-off-by: Antonio Murdaca <antoniomurdaca@gmail.com>

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-curl -O https://dl.fedoraproject.org/pub/fedora/linux/releases/38/Everything/x86_64/os/images/boot.iso
+FILE="$PWD"/boot.iso
+if [ ! -f "$FILE" ]; then
+    curl -O https://dl.fedoraproject.org/pub/fedora/linux/releases/38/Everything/x86_64/os/images/boot.iso
+fi
 virt-install --connect qemu:///system --name sagano-demo --memory 2048 --vcpus 4 --disk size=40 \
           --boot loader=/usr/share/edk2/ovmf/OVMF_CODE.secboot.fd,loader.readonly=yes,loader.secure='no',loader.type=pflash,nvram=/var/lib/libvirt/qemu/nvram/sagano-demo_VARS.fd --network=network=default,model=virtio \
           --os-variant rhel9.0 --location boot.iso \
